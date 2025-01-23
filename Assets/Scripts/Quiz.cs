a00165059063281d9cbaa5b705994a8dadd3982e
Ana Kniss <ackniss@gmail.com>
2025-01-22 23:58:48 -0300

    add progress bar

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -29,11 +29,18 @@ public class Quiz : MonoBehaviour
     [Header("Scoring")] 
     [SerializeField] TextMeshProUGUI scoreText;
     ScoreKeeper scoreKeeper;
+
+    [Header("ProgressBar")] 
+    [SerializeField] Slider progressBar;
+
+    public bool isComplete;
     
     void Start()
     {
         timer = FindObjectOfType<Timer>();
         scoreKeeper = FindObjectOfType<ScoreKeeper>();
+        progressBar.maxValue = questions.Count;
+        progressBar.value = 0;
     }
 
     void Update()
@@ -60,6 +67,11 @@ public class Quiz : MonoBehaviour
         SetButtonState(false);
         timer.CancelTimer();
         scoreText.text = "Score: " + scoreKeeper.CalculateScore() + "%";
+
+        if (progressBar.value == progressBar.maxValue)
+        {
+            isComplete = true;
+        }
     }
 
     void DisplayAnswer(int index)
@@ -91,6 +103,7 @@ public class Quiz : MonoBehaviour
             SetDefaultButtonSprites();
             GetRandomQuestion();
             DisplayQuestion();
+            progressBar.value++;
             scoreKeeper.IncrementQuestionsSeen();
         }
     }
